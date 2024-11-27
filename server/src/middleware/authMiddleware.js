870c73840aa1c6fbe5570ad86c56c06e615f10b2
nhlanhla <roykeane888@gmail.com>
2024-11-27 10:22:36 +0200

    updated middleware

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -1,42 +1,27 @@
+
+
 const jwt = require('jsonwebtoken');
-const Admin = require('../models/admin');
 const User = require('../models/user');
 
-const authMiddleware = (role) => async (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    // const token = req.cookies.auth_token;
+
+    if (!token) {
+        return res.status(401).json({ error: 'Authorization token is required' });
+    }
+
     try {
-        // Authorization header
-        const token = req.header('Authorization').replace('Bearer ', '');
-        if (!token) {
-            return res.status(401).json({ error: 'Access denied. No token provided.' });
-        }
-
-        // Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        // Fetch user based on the role
-        if (role === 'admin') {
-            const admin = await Admin.findById(decoded._id);
-            if (!admin) throw new Error('Admin not found');
-            req.admin = admin;
-        } else if (role === 'user') {
-            const user = await User.findById(decoded._id);
-            if (!user) throw new Error('Access denied. User not found');
-            req.user = user;
-        } else {
-            return res.status(400).json({error: 'Invalid role'});
+        req.user = await User.findById(decoded.id);  // Use decoded.id instead of decoded._id
+        if (!req.user) {
+            return res.status(404).json({ error: 'User not found' });
         }
-
-        // Route handler
+        next();
     } catch (error) {
-        if (error.message === 'No token provided') {
-            return res.status(401).json({ error: 'No token provided'});
-        } else if (error.message === 'jwt malformed'){
-            return res.status(401).json({ error: 'Invalid token'});
-        } else if (error.message === 'Admin not found' || error.message === 'User not found') {
-            return res.status(401).json({ error: 'Authentication failed'});
-        }
-            return res.status(500).json({ error: 'Internal server error'});
+        console.error('Token verification error:', error);  // Log the error for debugging
+        return res.status(401).json({ error: 'Invalid token' });
     }
 };
 
-module.exports = authMiddleware
+module.exports = authMiddleware;
