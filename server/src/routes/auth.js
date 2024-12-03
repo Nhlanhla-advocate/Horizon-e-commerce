a6ed14ecab4551aad41a24e4ce84d43d217a11e5
nhlanhla <roykeane888@gmail.com>
2024-12-03 11:01:52 +0200

    .env changes

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
-// const authController = require('../controllers/authController');
+const authController = require('../controllers/authController');
 // const { validateSignIn, validateSignUp, validate } = require('../utilities/validation')
 
-router.post('/signup', (req, res) => {
-    res.send('Sighup route');
-});
+router.post('/signup', authController.signUp);
 
-router.post('/signin', (req, res) => {
-    res.send('Sigin route');
-});
+router.post('/signin', authController.signIn);
 
-router.post('/signout', (req, res) => {
-    res.send('Signout route');
-});
+
+router.post('/signout', authController.signOut);
 
 module.exports = router;
