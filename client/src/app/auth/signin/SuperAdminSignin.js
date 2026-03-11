5cc7bf067ae422fbcb620f260af97e8cd2e7bb4d
Nhlanhla Advocate Msibi <168407572+Nhlanhla-advocate@users.noreply.github.com>
2026-03-11 15:49:31 +0200

    URL

--- client/src/app/auth/signin/SuperAdminSignin.js
+++ client/src/app/auth/signin/SuperAdminSignin.js
@@ -6,3 +6,6 @@ import styles from '../../assets/css/auth.module.css';
 import '../../assets/css/buttons.css';
 import Link from 'next/link';
 
+const API_BASE = process.env.NEXT_PUBLIC_API_URL
+|| 'http://localhost:5000';
+
