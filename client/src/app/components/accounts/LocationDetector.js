1cac23e18b137d99546520c1ec519324902967b6
Nhlanhla Advocate Msibi <168407572+Nhlanhla-advocate@users.noreply.github.com>
2026-06-04 14:16:26 +0200

    imports and free API

--- client/src/app/components/accounts/LocationDetector.js
+++ client/src/app/components/accounts/LocationDetector.js
@@ -1,1 +1,12 @@
 
+'use client';
+
+import { useState } from 'react';
+import {
+    getCurrencyLabel,
+    getLanguageLabel,
+    getLocaleForCountry,
+} from './localeData';
+
+//Fre, key-less, CORS-enabled reverse geocoding designed for browser use.
+const REVERSE_GEOCODE_URL =  'https://api.bigdatacloud.net/data/reverse-geocode-client';
